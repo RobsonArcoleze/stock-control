870d8f26f0a9e76f9740c0eb43441eea2f4ae4c2
RobsonArcoleze <robson.arcoleze@artesanalinvestimetos.com.br>
2023-07-24 22:32:50 -0300

    Feature: Implementando Formulários Reativos
    - Aplicado estilização padrão
    - Importado a classe formBuilder
    - Criado um formGroup setando os nomes dos inputs
    - Utilizado o método do (ngSubmit)
        *Obs: Não foi preciso colocar (click)=metodo no botão, o type do botão deve ser sbmit
    - Utilizado o formControlName para referenciar os valores dos inputs com o formBuilder.

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,34 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   loginCard = true;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ){}
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.compose([
+      Validators.required,
+      Validators.email
+    ])],
+    password: ['', Validators.required],
+
+  })
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.compose([
+      Validators.required,
+      Validators.email
+    ])],
+    password: ['', Validators.required],
+  })
+
+  onSubmitLoginForm(): void {
+    console.log('Dados do Formulário de Login: ', this.loginForm.value);
+  }
+
+  onSubmitSignupForm(): void {
+    console.log('Dados do Formulário de Login: ', this.signupForm.value);
+  }
 }
